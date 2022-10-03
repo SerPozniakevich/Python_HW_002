5cf1d017daf53085aa996b7d61915591f6b4aa7b
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-03 20:41:08 +0700

    add solution task 004 Albina

--- Task_004.py
+++ Task_004.py
@@ -2,14 +2,35 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-with open("file.txt") as file: # выгрузка значений из файла.
-    x = int(file.readline())
-    y = int(file.readline())
-    z = int(file.readline())
+#Var my
 
-n = int(input('Введите N: '))
-line_n = range(-n, n+1)        # заполнение строки по параметрам
-for i in line_n:
-    print(i, end=', ')
-print(f'\n{x}, {y}, {z}')
-print(f'{line_n[x]*line_n[y]*line_n[z]}')
+# with open("file.txt") as file: # выгрузка значений из файла.
+#     x = int(file.readline())
+#     y = int(file.readline())
+#     z = int(file.readline())
+
+# n = int(input('Введите N: '))
+# line_n = range(-n, n + 1)        # заполнение строки по параметрам
+# for i in line_n:
+#     print(i, end=', ')
+# print(f'\n{x}, {y}, {z}')
+# print(f'{line_n[x]*line_n[y]*line_n[z]}')
+
+# Var Albina
+
+from unittest import result
+
+
+pos = []                         # создание списка для файла
+with open("file.txt") as f:      # открываем файл
+    pos = f.read().split()      # read - чтение всего файла, split - разбивка данных в файле по пробелам и переводам строк 
+    pos = [int(i) for i in pos] # записывание данных из файла в виде списка в симействе int (внутренним циклом for)
+n = int(input('Введите целое число: '))
+s = []
+for i in range(-n, n + 1):
+    s.append(i)
+result = 1
+for i in range(len(pos)):
+    result *= s[pos[i]]
+print(f'Исходный список: {s}.\nПозиции: {pos}.')
+print(result)
